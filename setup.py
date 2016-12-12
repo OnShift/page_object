a15945c2f6a8268cdddb2daf31a304e51066c3b2
Doug Morgan <dmorgan@onshift.com>
2016-12-12 09:29:47 -0500

    Bumping Version to 0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="page_object",
-    version="0.0.6",
+    version="0.0.7",
     description='PageObject pattern python implementation',
     packages=find_packages('src'),
     package_dir={'': 'src'},
