2e71212b2dedab9110cb58d4d2cac2cd39625973
Doug Morgan <dmorgan@onshift.com>
2017-01-06 14:15:03 -0500

    Fixing up Setup.py for Pypi

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="page_object",
-    version="0.1.0",
-    description='PageObject pattern python implementation',
+    version="0.1.1",
+    description="Python implementation of the PageObject pattern",
+    author="Onshift Inc.",
+    author_email="dev@onshift.com",
+    url="https://github.com/OnShift/page_object",
+    license="MIT",
+    keywords=['page_object', 'selenium', 'webdriver'],
     packages=find_packages('src'),
     package_dir={'': 'src'},
-
     install_requires=[
         'pyvirtualdisplay==0.2',
         'selenium==3.0.2'
