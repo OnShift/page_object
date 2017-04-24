45359d8cc54a36e2bece66a182fa295fd6bae09c
Doug Morgan <dmorgan@onshift.com>
2017-04-24 11:00:46 -0400

    Bumping Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="page_object",
-    version="0.2.0",
+    version="0.2.1",
     description="Python implementation of the PageObject pattern",
     author="Onshift Inc.",
     author_email="dev@onshift.com",
