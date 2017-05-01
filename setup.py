56ee3512fabc8844bb5bff4918b7c957dace9a3c
Jim Hribar <jhribar@onshift.com>
2017-05-01 14:44:57 -0400

    Change WebDriverWait poll_frequency from 500ms to 100ms

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="page_object",
-    version="0.2.1",
+    version="0.2.2",
     description="Python implementation of the PageObject pattern",
     author="Onshift Inc.",
     author_email="dev@onshift.com",
