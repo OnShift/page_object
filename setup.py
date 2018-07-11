bd2ebb06318342f1364be7017cc767b94496f57d
jbowers <jbowers@onshift.com>
2018-07-11 16:05:35 -0400

    bump default timeouts to 15

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="page_object",
-    version="0.3.2",
+    version="0.3.3",
     description="Python implementation of the PageObject pattern",
     author="Onshift Inc.",
     author_email="dev@onshift.com",
